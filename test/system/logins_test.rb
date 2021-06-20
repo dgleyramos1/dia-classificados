d18790dc55de9713ba90d8fe39ab5ad77d3074aa
dgleyramos1 <dgleysilva@gmail.com>
2021-06-20 04:48:53 -0300

    Adicionando Autenticação para os usuários

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -1,9 +1,17 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit logins_url
-  #
-  #   assert_selector "h1", text: "Logins"
-  # end
+  test "Login com credencias validas" do
+    user = FactoryBot.create (:user, password: "password", password_confirmation: "password")
+
+    visit root_path
+    click_link "Entrar"
+
+    fill_in "E-mail", with: user.email
+    fill_in "Semha", with: "password"
+
+    click_buttom "Entrar"
+
+    assert_text "Bem vindo Darth Vader!"
+  end
 end
