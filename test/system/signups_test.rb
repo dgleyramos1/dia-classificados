951f1f2722da37434b5aad50bb71ff7a1ad9926b
dgleyramos1 <dgleysilva@gmail.com>
2021-06-20 01:50:08 -0300

    Realizando test:sytem no cadastro de usuários

--- test/system/signups_test.rb
+++ test/system/signups_test.rb
@@ -5,13 +5,22 @@ class SignupsTest < ApplicationSystemTestCase
     visit "/" 
     click_on "Cadastre-se"
 
-    fill_in "Nome", with: "Darth Vader"
-    fill_in "E-mail", with: "vader@imperio.com"
+    fill_in "Nome", with: "Dgley ramos"
+    fill_in "E-mail", with: "ramos@dev.com"
     fill_in "Senha", with: "minhasenha"
-    fill_in "Confirme sua senha", with: "minhasenha"
+    fill_in "Confirmação de senha", with: "minhasenha"
 
     clink_on "Cadastrar"
 
     assert_text "Cadastro realizados com sucesso!"
+  end
+
+  test "usuario faz cadastro com dados invalidos" do
+    visit "/"
+    click_on "Cadastre-se"
+    click_on "Cadastrar"
+
+
+    assert_text "Erro ao salvar"
   end
 end
