edf96c0c4d59e5c69a63b344602c79b9bf843114
dgleyramos1 <dgleysilva@gmail.com>
2021-06-20 02:10:05 -0300

    Validando campos de cadastro de usuário

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "nome é obrigatorio" do
+    user = User.new(name: nil)
+
+    user.save
+
+    assert user.errors[:name],any?
+  end
+  test "email é obrigatorio e no formato valido" do
+    user = User.new(email: nil)
+    user.save
+
+    assert user.errors[:email],any?
+
+    user = User.new(email: "Invalido")
+    user.save
+
+    assert user.errors[:email],any?
+
+    user = User.new(email: "valido@exemplo.com")
+    user.save
+
+    refute user.errors[:email],any?
+  end
+  test "email é unico" do
+    user = User.create!(name: "Darth Vader" , email: "vader@exemplo.com", password: "secret", password_confirmation: "secret")
+
+    other_user = User.create(email: "vader@exemplo.com")
+
+    assert other_user.errors[:email].any?
+  end
 end
